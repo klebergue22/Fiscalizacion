209852ca4a5bead4e765cef847effb8886400c60
klebergue22 <51449689+klebergue22@users.noreply.github.com>
2026-05-04 16:32:07 -0500

    Separar reporte ocho horas semanal en controlador y servicio propios

---
+++ Fiscalizacion-web/src/main/java/gob/igm/ec/reportes/ReporteOchoHorasSemanalControlador.java
@@ -0,0 +1,106 @@
+package gob.igm.ec.reportes;
+
+import gob.igm.ec.reportes.servicio.ReporteOchoHorasSemanalServicio;
+import gob.igm.ec.util.FacesUtil;
+import gob.igm.ec.util.JasperReportUtil;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+import java.util.Date;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletResponse;
+import org.primefaces.model.StreamedContent;
+
+@Named
+@SessionScoped
+public class ReporteOchoHorasSemanalControlador extends FacesUtil implements Serializable {
+
+    private StreamedContent media;
+    private ByteArrayOutputStream outputStream;
+    private boolean renderBarra;
+    private String path;
+    private Date fechaDesde;
+
+    @Inject
+    private ReporteOchoHorasSemanalServicio reporteOchoHorasSemanalServicio;
+
+    public ReporteOchoHorasSemanalControlador() {
+        this.renderBarra = false;
+        this.path = JasperReportUtil.PATH_REPORTE_OCHO_HORAS_SEMANAL;
+    }
+
+    public void generarReporteOchoHorasSemanal() {
+        try {
+            this.renderBarra = true;
+
+            if (fechaDesde == null) {
+                FacesContext.getCurrentInstance().addMessage(null,
+                        new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "DEBE SELECCIONAR FECHA"));
+                return;
+            }
+
+            outputStream = reporteOchoHorasSemanalServicio.generar(fechaDesde);
+            media = JasperReportUtil.getStreamContentFromOutputStream(outputStream, "application/pdf", getNameFilePdf());
+        } catch (Exception e) {
+            //log.error(e.getMessage(), e);
+        }
+    }
+
+    public String getNameFilePdf() {
+        return "reporteOchoHorasSemanal";
+    }
+
+    public void downloadFile() {
+        try {
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
+            response.reset();
+            response.setContentType("application/pdf");
+            response.setHeader("Content-disposition", "attachment; filename=" + getNameFilePdf());
+
+            OutputStream output = response.getOutputStream();
+            output.write(outputStream.toByteArray());
+            output.close();
+
+            facesContext.responseComplete();
+        } catch (Exception e) {
+            //log.error(e.getMessage(), e);
+        }
+    }
+
+    public StreamedContent getMedia() {
+        return media;
+    }
+
+    public void setMedia(StreamedContent media) {
+        this.media = media;
+    }
+
+    public boolean isRenderBarra() {
+        return renderBarra;
+    }
+
+    public void setRenderBarra(boolean renderBarra) {
+        this.renderBarra = renderBarra;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Date getFechaDesde() {
+        return fechaDesde;
+    }
+
+    public void setFechaDesde(Date fechaDesde) {
+        this.fechaDesde = fechaDesde;
+    }
+}
